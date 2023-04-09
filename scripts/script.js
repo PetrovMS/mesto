67998a4295766c83f4bf2646fa95acfe94c13df1
Mikhail Petrov <misha.petrov1509@mail.ru>
2023-04-09 22:06:35 +0300

    Внесены правки

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
 let openForm = document.querySelector('.profile__edit-button');
 let closeForm = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
-let inputName = document.querySelector('.popup__name');
-let inputSelf = document.querySelector('.popup__self');
+let inputName = document.querySelector('.popup__input_Name');
+let inputSelf = document.querySelector('.popup__input_Self');
 let profileName = document.querySelector('.profile__title');
 let profileSelf = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.popup');
+let formElement = document.querySelector('form');
 
 function openWindow() {
     popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputSelf.value = profileSelf.textContent;
 }
 openForm.addEventListener('click', openWindow);
 
@@ -21,5 +23,6 @@ function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileSelf.textContent = inputSelf.value;
+    popup.classList.remove('popup_opened');
 }
 formElement.addEventListener('submit', handleFormSubmit);
